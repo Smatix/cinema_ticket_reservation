16418aa1bef213ab8c8149ec3a5b6b99e5e2bbaf
Smatix <suchenia.mateusz@gmail.com>
2021-03-28 23:27:42 +0200

    Add sleep in bash

--- bin/build.sh
+++ bin/build.sh
@@ -1,5 +1,7 @@
 docker-compose build
 docker-compose up -d
+echo "wait for dockers..."
+sleep 10
 docker exec cinema_web composer install
 docker exec cinema_web php bin/console doctrine:database:create --if-not-exists
 docker exec cinema_web php bin/console doctrine:migrations:migrate --no-interaction
